23dd17d4c0a3142df98cfa508fb4c3376e708894
jidicula <johanan@forcepush.tech>
2021-03-27 19:18:25 -0400

    test: Add table test for getSortedKeys()
    Resolves: #3

--- random-standup_test.go
+++ random-standup_test.go
@@ -2,7 +2,10 @@ package main
 
 import (
 	"fmt"
+	"reflect"
 	"testing"
+
+	"github.com/pelletier/go-toml"
 )
 
 func TestShuffleTeam(t *testing.T) {
@@ -21,6 +24,42 @@ func TestShuffleTeam(t *testing.T) {
 		t.Run(testname, func(t *testing.T) {
 			output := shuffleTeam(tt.teamMembers, tt.teamName)
 			if output != tt.want {
+				t.Errorf("got %s, want %s", output, tt.want)
+			}
+		})
+	}
+}
+
+func TestGetSortedKeys(t *testing.T) {
+
+	emptySubteams, _ := toml.Load(`
+["subteam 1"]
+
+[subteam-2]
+
+["subteam 3"]`)
+
+	mixedSubteams, _ := toml.Load(`
+["subteam 1"]
+members = []
+[subteam-2]
+members = ["Alice", "Bob"]
+["subteam 3"]`)
+
+	var tests = []struct {
+		roster *toml.Tree
+		want   []string
+	}{
+
+		{emptySubteams, []string{"subteam 1", "subteam-2", "subteam 3"}},
+		{mixedSubteams, []string{"subteam 1", "subteam-2", "subteam 3"}},
+	}
+
+	for _, tt := range tests {
+		testname := tt.roster.String()
+		t.Run(testname, func(t *testing.T) {
+			output := getSortedKeys(tt.roster)
+			if !reflect.DeepEqual(output, tt.want) {
 				t.Errorf("got %s, want %s", output, tt.want)
 			}
 		})
