d1820bb91e30720026c328fda4dbe7e18e88889a
jidicula <johanan@forcepush.tech>
2021-03-27 21:18:52 -0400

    test: Add tests for standupOrder()
    Resolves: #3

--- random-standup_test.go
+++ random-standup_test.go
@@ -65,3 +65,89 @@ members = ["Alice", "Bob"]
 		})
 	}
 }
+
+func TestStandupOrder(t *testing.T) {
+	singleSubteam := `
+[Subteam-1]
+members = [
+        "Alice",
+        "Bob",
+        "Carol",
+        "David"
+        ]
+`
+	subteamPair := singleSubteam + "\n" + `["Subteam 2"]
+members = ["Erin", "Frank", "Grace", "Heidi"]`
+
+	lastSubteamEmpty := subteamPair + "\n" + `
+
+["Empty Subteam"]
+`
+	middleSubteamEmpty := singleSubteam + "\n" + `
+
+["Empty Subteam"]
+` + "\n" + `["Subteam 2"]
+members = ["Erin", "Frank", "Grace", "Heidi"]`
+
+	var tests = []struct {
+		roster string
+		want   string
+	}{
+		{singleSubteam, `## Subteam-1
+Alice
+Carol
+David
+Bob
+`},
+		{subteamPair, `## Subteam-1
+Bob
+Carol
+David
+Alice
+
+## Subteam 2
+Erin
+Grace
+Heidi
+Frank
+`},
+		{lastSubteamEmpty, `## Subteam-1
+Carol
+David
+Bob
+Alice
+
+## Subteam 2
+Heidi
+Grace
+Erin
+Frank
+
+`}, // trailing newline is workaround
+		{middleSubteamEmpty, `## Subteam-1
+Bob
+David
+Alice
+Carol
+
+## Subteam 2
+Erin
+Grace
+Heidi
+Frank
+`},
+	}
+
+	for _, tt := range tests {
+		testname := tt.roster
+
+		t.Run(testname, func(t *testing.T) {
+			rosterTree, _ := toml.Load(tt.roster)
+			output := standupOrder(rosterTree)
+			if output != tt.want {
+				t.Errorf("got %s, want %s", output, tt.want)
+			}
+		})
+	}
+
+}
